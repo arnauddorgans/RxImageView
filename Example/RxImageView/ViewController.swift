69ca5960a7e25a77321facde11a97a0733e81884
Arnaud Dorgans <ineox@me.com>
2017-08-30 16:15:00 +0200

    0.1.4

--- Example/RxImageView/ViewController.swift
+++ Example/RxImageView/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxImageView
+import PureLayout
 
 class ViewController: UIViewController {
+    
+    let imageView = UIImageView()
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = .black
+        
+        imageView.contentMode = .scaleAspectFit
+        self.view.addSubview(imageView)
+        imageView.autoPinEdgesToSuperviewEdges()
+        
+        setupRx()
+    }
+    
+    func setupRx() {
+        
+        Observable<Int>.interval(2, scheduler: MainScheduler.instance)
+            .map{ URL(string: "https://lorempixel.com/400/200/cats/?\($0)=\($0)") }
+            .bind(to: imageView.rx.imageURL)
+            .disposed(by: disposeBag)
     }
 }
-
